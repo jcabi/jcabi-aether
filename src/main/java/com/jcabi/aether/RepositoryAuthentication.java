db583577bb5e76f4ecd5198271a3bd80b54366c2
oruam85 <oruam85@gmail.com>
2015-01-11 13:23:06 -0430

    39: create a new classes to encapsulate all necessary properties from

---
+++ src/main/java/com/jcabi/aether/RepositoryAuthentication.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2012-2014, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jcabi.aether;
+
+import org.sonatype.aether.repository.Authentication;
+
+import com.jcabi.aspects.Immutable;
+
+/**
+ * Parameter holder for org.sonatype.aether.repository.Authentication.
+ * 
+ * @author Mauricio Herrera (oruam85@gmail.com)
+ * @version $Id$
+ */
+public final class RepositoryAuthentication {
+
+	/**
+	 * The user name
+	 */
+	private final String username;
+
+	/**
+	 * The password
+	 */
+	@Immutable.Array
+	private final char[] password;
+
+	/**
+	 * The path to the private key file
+	 */
+	private final String privateKeyFile;
+
+	/**
+	 * The passphrase for the private key file
+	 */
+	@Immutable.Array
+	private final char[] passphrase;
+
+	public RepositoryAuthentication(Authentication auth) {
+		this.username = auth.getUsername();
+		this.password = auth.getPassword().toCharArray();
+		this.privateKeyFile = auth.getPrivateKeyFile();
+		this.passphrase = auth.getPassphrase().toCharArray();
+	}
+
+	/**
+	 * Getter of the username attribute
+	 * 
+	 * @return The username
+	 */
+	public String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * Getter of the password attribute
+	 * 
+	 * @return The password
+	 */
+	public char[] getPassword() {
+		return this.password;
+	}
+
+	/**
+	 * Getter of the privateKeyFile attribute
+	 * 
+	 * @return The privateKeyFile
+	 */
+	public String getPrivateKeyFile() {
+		return this.privateKeyFile;
+	}
+
+	/**
+	 * Getter of the passphrase attribute
+	 * 
+	 * @return The passphrase
+	 */
+	public char[] getPassphrase() {
+		return this.passphrase;
+	}
+
+}
